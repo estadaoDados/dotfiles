c234b358e55a804aa7c91fa14ddbb2b355adf6e8
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-06-06 21:21:30 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 
+echo "1 - Instalando pacotes básicos iniciais"
+sudo aptitude install ruby rdoc irb git
+
+echo "2 - Baixando RubyGems no diretório ~/tools/"
+mkdir -p ~/tools
+cd ~/tools
+wget http://production.cf.rubygems.org/rubygems/rubygems-2.2.2.tgz
+
+echo "3 - Extraindo o pacote RubyGems"
+tar xzf rubygems-2.2.2.tgz
+
+echo "4 - Instalando o RubyGems"
+cd rubygems-2.2.2
+sudo ruby setup.rb all
+
+echo "5 - Instalando o homesick"
+sudo gem install homesick
+
+echo "6 - Clonando o repositório do estadão dados para dotfiles"
+homesick clone https://github.com/estadaodados/dotfiles.git
+
 echo "Instalando ctags"
 echo "sudo apt-get install exuberant-ctags"
 sudo apt-get install exuberant-ctags
 git submodule update --init
 homesick symlink dotfiles
 
-
 git_uri='https://github.com/estadaoDados/spf13-vim.git'
 git_branch='3.0'
 VUNDLE_URI="https://github.com/gmarik/vundle.git"
-
 
 lnif() {
   if [ -e "$1" ]; then
